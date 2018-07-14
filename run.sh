132bec82166355fb4ef2e380224aedece5fe763a
zhangjunyi <1225054736@qq.com>
2018-07-14 15:36:24 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+sudo apt-get install ctags
+sudo apt-get install vim-addon-manager
+vim-addons install omnicppcomplete
+vim-addons install taglist
+vim-addons install winmanager
+vim-addons install minibufexplorer
